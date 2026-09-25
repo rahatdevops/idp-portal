87ce93fcd82a8cf9572d3e20b68d85e275b159c9
rahatdevops <saeed_456@outlook.com>
2026-09-25 19:56:30 +0600

    update

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,8 +1,17 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha';
+import scaffolderPlugin from '@backstage/plugin-scaffolder/alpha';
+
 import { navModule } from './modules/nav';
 import { homeModule } from './modules/home';
 
 export default createApp({
-  features: [catalogPlugin, navModule, homeModule],
+  features: [
+    catalogPlugin,
+    kubernetesPlugin,
+    scaffolderPlugin,
+    navModule,
+    homeModule,
+  ],
 });
